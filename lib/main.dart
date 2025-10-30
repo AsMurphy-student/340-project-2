f1e1ef6482644b0381151e5fe1992d6d08568a03
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-10-30 15:07:32 -0400

    Finished project

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:http/http.dart' as http;
 import 'dart:core';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -14,7 +16,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Home());
+    return const MaterialApp(home: Home(), debugShowCheckedModeBanner: false);
   }
 }
 
@@ -26,18 +28,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // Lists
   List<String> mainBreeds = [];
   List<String> subBreeds = [];
   List<String> results = [];
 
-  String selectedBreed = '';
-  String selectedSubBreed = 'N/A';
+  // Selected options
+  String? selectedBreed;
+  String? selectedSubBreed;
 
+  // Num Of Results
   int numOfResults = 5;
+  // Current Num of Results which is updated after submit button is clicked
   int currentNumOfResults = 5;
 
+  // Text Controller for Number of Results
+  final TextEditingController numOfResultsController = TextEditingController(
+    text: '5',
+  );
+
+  // Preferences
   late SharedPreferences myPrefs;
 
+  // Preference Functions
   Future<void> initPrefs() async =>
       myPrefs = await SharedPreferences.getInstance();
 
@@ -54,10 +67,7 @@ class _HomeState extends State<Home> {
     currentNumOfResults = numOfResults;
   });
 
-  final TextEditingController numOfResultsController = TextEditingController(
-    text: '5',
-  );
-
+  // Get Breeds Function to populate dropdown
   Future<void> getBreeds(String fetchURL) async {
     // Get response and assign variables accordingly
     var response = await http.get(Uri.parse(fetchURL));
@@ -68,7 +78,6 @@ class _HomeState extends State<Home> {
         List<dynamic> data = jsonResponse['message'];
         setState(() {
           mainBreeds = data.map((element) => element.toString()).toList();
-          selectedBreed = mainBreeds[0];
         });
       }
     } else {
@@ -76,6 +85,7 @@ class _HomeState extends State<Home> {
     }
   }
 
+  // Get sub breeds function to populate subbreed dropdown
   Future<void> getSubBreeds() async {
     // Get response and assign variables accordingly
     var response = await http.get(
@@ -90,7 +100,7 @@ class _HomeState extends State<Home> {
           subBreeds = data.isNotEmpty
               ? data.map((element) => element.toString()).toList()
               : [];
-          selectedSubBreed = subBreeds.isNotEmpty ? subBreeds[0] : 'N/A';
+          selectedSubBreed = subBreeds.isNotEmpty ? subBreeds[0] : null;
         });
       }
     } else {
@@ -98,14 +108,40 @@ class _HomeState extends State<Home> {
     }
   }
 
+  // Get images and fill up image url array
   Future<void> getImages() async {
+    // If no breed selected show error dialog to user
+    if (selectedBreed == null) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("No Breed Selected"),
+            content: Text(
+              "You need to select at least one breed to search for.",
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text("OK"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
     // Set appropriate fetch url
-    String fetchURL = selectedSubBreed != 'N/A'
+    String fetchURL = selectedSubBreed != null
         ? "https://dog.ceo/api/breed/$selectedBreed/$selectedSubBreed/images/random/$numOfResults"
         : "https://dog.ceo/api/breed/$selectedBreed/images/random/$numOfResults";
 
     var response = await http.get(Uri.parse(fetchURL));
 
+    // If good response fill up results list
     if (response.statusCode == 200) {
       var jsonResponse = jsonDecode(response.body);
       if (jsonResponse['status'] == 'success') {
@@ -118,6 +154,7 @@ class _HomeState extends State<Home> {
           }
           currentNumOfResults = numOfResults;
         });
+        // Save prefs of num of results
         await saveNumOfResults();
       }
     } else {
@@ -125,13 +162,20 @@ class _HomeState extends State<Home> {
     }
   }
 
+  // Validate search function for results text field
+  // If field is empty or has letters and not numbers throw an error
   String? validateSearch(String value) {
     if (value.isEmpty) return 'Field cannot be empty';
-    if (value.contains(RegExp(r'[a-zA-Z]')))
+    if (value.contains(RegExp(r'[a-zA-Z]'))) {
       return 'Field needs to be a number';
+    }
     return null;
   }
 
+  // Async init function
+  // Loads breed types
+  // Inits prefs
+  // Load num of results if saved
   Future<void> initFunc() async {
     await getBreeds("https://dog.ceo/api/breeds/list");
     await initPrefs();
@@ -147,16 +191,52 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Project 2 Dog API")),
+      appBar: AppBar(
+        title: Text("Project 2 Dog API"),
+        actions: [
+          // Documentation Button
+          IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text("Project Information"),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextButton(onPressed: () => launchUrl(Uri.parse('https://dog.ceo/dog-api/')), child: Text("Dog API")),
+                        Text(
+                          "I went ahead and focused on getting API calls working correctly then focused on getting the grid working with the UI elements. After this I finally focused on polishing the app a bit.",
+                        ),
+                      ],
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text("OK"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            icon: Icon(Icons.info),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           children: [
+            // Row containing breed and sub breed dropdowns
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 DropdownButton<String>(
                   value: selectedBreed,
-                  // hint: Text("Rating (allows set rating and below)"),
+                  hint: Text("Breed"),
                   icon: const Icon(Icons.arrow_downward),
                   items: mainBreeds.isNotEmpty
                       ? mainBreeds.map<DropdownMenuItem<String>>((
@@ -174,10 +254,6 @@ class _HomeState extends State<Home> {
                           );
                         }).toList(),
                   onChanged: (String? newValue) {
-                    // setState(() {
-                    //   rating = newValue!;
-                    // });
-                    // print(newValue);
                     selectedBreed = newValue!;
                     getSubBreeds();
                   },
@@ -185,7 +261,7 @@ class _HomeState extends State<Home> {
 
                 DropdownButton<String>(
                   value: selectedSubBreed,
-                  // hint: Text("Rating (allows set rating and below)"),
+                  hint: Text("SubBreed"),
                   icon: const Icon(Icons.arrow_downward),
                   items: subBreeds.isNotEmpty
                       ? subBreeds.map<DropdownMenuItem<String>>((String value) {
@@ -208,29 +284,40 @@ class _HomeState extends State<Home> {
                 ),
               ],
             ),
+
+            // Number of results field
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
-                  child: TextField(
-                    controller: numOfResultsController,
-                    decoration: InputDecoration(
-                      label: Text('Search Term'),
-                      errorText: validateSearch(
-                        numOfResultsController.value.text,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: numOfResultsController,
+                      decoration: InputDecoration(
+                        label: Text('Number of Results'),
+                        errorText: validateSearch(
+                          numOfResultsController.value.text,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: Colors.black),
+                        ),
                       ),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(width: 2, color: Colors.black),
-                      ),
+                      onChanged: (value) => setState(() {
+                        numOfResults = int.parse(value) <= 50
+                            ? int.parse(value)
+                            : 50;
+                      }),
                     ),
-                    onChanged: (value) => setState(() {
-                      numOfResults = int.parse(value);
-                    }),
                   ),
                 ),
-                TextButton(onPressed: () => getImages(), child: Text("Submit")),
               ],
             ),
+
+            // Submit Button
+            TextButton(onPressed: () => getImages(), child: Text("Submit")),
+
+            // Grid of images
             Expanded(
               child: GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -240,12 +327,6 @@ class _HomeState extends State<Home> {
                 ),
                 itemCount: currentNumOfResults,
                 itemBuilder: (context, index) {
-                  // return Image.network(
-                  //   results.isNotEmpty
-                  //       ? results[index]
-                  //       : 'https://placehold.co/400.png',
-                  //   fit: BoxFit.cover,
-                  // );
                   return CachedNetworkImage(
                     imageUrl: results.isNotEmpty
                         ? results[index]
